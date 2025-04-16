a0aeaa15aec93f797e7bbfcabb7f500e4d9c158f
pikman34 <noahbob34@gmail.com>
2025-04-16 18:11:11 +0100

    puzzle fully completeable

--- CATNAPPED/Assets/Scripts/PipeScript.cs
+++ CATNAPPED/Assets/Scripts/PipeScript.cs
@@ -7,19 +7,32 @@ public class PipeScript : MonoBehaviour
 
     float[] rotations = { 0,90,180,270 };
 
-    public float correctRotation;
+    public float[] correctRotation;
     [SerializeField]
     bool isPlaced = false;
 
+    int PossibleRots = 1;
+
     private void Start()
     {
+        PossibleRots = correctRotation.Length;
         int rand = Random.Range(0, rotations.Length);
         transform.eulerAngles = new Vector3(0, 0, rotations[rand]);
 
-                if(transform.eulerAngles.z == correctRotation) 
+        if(PossibleRots > 1) 
         {
-            isPlaced = true;
+            if(transform.eulerAngles.z == correctRotation[0] || transform.eulerAngles.z == correctRotation[1]) 
+            {
+                isPlaced = true;
+            }
         }    
+        else 
+        {
+            if(transform.eulerAngles.z == correctRotation[0]) 
+            {
+                isPlaced = true;
+            }
+        }
     }
 
     private void OnMouseDown()
@@ -27,13 +40,27 @@ public class PipeScript : MonoBehaviour
         transform.Rotate(0, 0, 90);
         transform.eulerAngles = new Vector3(0, 0, Mathf.Round(transform.eulerAngles.z));   
 
-        if(transform.eulerAngles.z == correctRotation && isPlaced == false) 
+    if(PossibleRots > 1) 
         {
-            isPlaced = true;
-        }    
-        else if(isPlaced == true) 
+            if(transform.eulerAngles.z == correctRotation[0] || transform.eulerAngles.z == correctRotation[1] && isPlaced == false) 
+            {
+                isPlaced = true;
+            }    
+            else if(isPlaced == true) 
+            {
+                isPlaced = false;
+            }
+        }
+        else 
         {
-            isPlaced = false;
+            if(transform.eulerAngles.z == correctRotation[0] && isPlaced == false) 
+            {
+                isPlaced = true;
+            }    
+            else if(isPlaced == true) 
+            {
+                isPlaced = false;
+            }
         }
     }
 }
